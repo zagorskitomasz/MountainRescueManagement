c462275f87519a7d391cc1fafb261b452daba6e0
Tomasz Zagórski <30413731+zagorskitomasz@users.noreply.github.com>
2017-11-23 19:11:17 +0100

    Add files via upload
    minor modifications + new tables for security

---
+++ sql/security.sql
@@ -0,0 +1,36 @@
+USE rescuecrm;
+DROP TABLE IF EXISTS user_role;
+DROP TABLE IF EXISTS role;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE role (
+  role_id int(11) NOT NULL AUTO_INCREMENT,
+  role varchar(255) DEFAULT NULL,
+  PRIMARY KEY (role_id)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user` (
+  user_id int(11) NOT NULL AUTO_INCREMENT,
+  active int(11) DEFAULT NULL,
+  email varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  PRIMARY KEY (user_id)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS user_role;
+CREATE TABLE user_role (
+  user_id int(11) NOT NULL,
+  role_id int(11) NOT NULL,
+  PRIMARY KEY (user_id,role_id),
+  KEY FKa68196081fvovjhkek5m97n3y (role_id),
+  CONSTRAINT FK859n2jvi8ivhui0rl0esws6o FOREIGN KEY (user_id) REFERENCES `user` (user_id),
+  CONSTRAINT FKa68196081fvovjhkek5m97n3y FOREIGN KEY (role_id) REFERENCES `role` (role_id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+INSERT INTO `role` VALUES (1,'ADMIN');
+INSERT INTO `role` VALUES (2,'USER');
